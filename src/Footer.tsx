2692cc453cf41d770296a083e8aadc5b66fd00ec
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-08-29 10:45:09 +0100

    deploy ready

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Footer() {
 	return (
 		<div className='h-[200px] bg-blue-700 fixed bottom-0 z-0 w-full flex flex-col justify-end text-white text-sm'>
